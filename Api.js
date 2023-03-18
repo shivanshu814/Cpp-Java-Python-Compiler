5fb0cabbb196e5902ebb115118e758005b72371a
Shivanshu Pathak <97324405+shivanshu814@users.noreply.github.com>
2023-03-18 20:13:08 +0530

    Python Compiler

---
+++ Api.js
@@ -0,0 +1,52 @@
+/** @format */
+
+/** @format */
+const express = require('express');
+const app = express();
+const bodyP = require('body-parser');
+const compiler = require('compilex');
+const options = { stats: true };
+compiler.init(options);
+app.use(bodyP.json());
+app.use(
+	'/codemirror/codemirror-5.65.12',
+	express.static(
+		'C:/Users/shiva/OneDrive - BENNETT UNIVERSITY/Private space/compiler/codemirror/codemirror-5.65.12'
+	)
+);
+app.get('/', function (req, res) {
+	res.sendFile(
+		'C:/Users/shiva/OneDrive - BENNETT UNIVERSITY/Private space/compiler/index.html'
+	);
+});
+app.post('/compile', function (req, res) {
+	var code = req.body.code;
+	var input = req.body.input;
+	var lang = req.body.lang;
+	try {
+		if (lang == 'Python') {
+			if (!input) {
+				var envData = { OS: 'windows' };
+				compiler.compilePython(envData, code, function (data) {
+					if (data.output) {
+						res.send(data);
+					} else {
+						res.send({ output: 'error' });
+					}
+				});
+			} else {
+				var envData = { OS: 'windows' };
+				compiler.compilePythonWithInput(envData, code, input, function (data) {
+					if (data.output) {
+						res.send(data);
+					} else {
+						res.send({ output: 'error' });
+					}
+				});
+			}
+		}
+	} catch (e) {
+		console.log('error');
+	}
+});
+app.listen(5000);
---
+++ documentation.md
@@ -0,0 +1,13 @@
+<!-- @format -->
+
+### Compiler code
+
+https://github.com/scriptnull/compilex
+
+### help full
+
+https://www.youtube.com/watch?v=BSP99_6yBDI&list=PL9lRHqeCagtv-EsnrBVDRDh0QDc2LReac&index=6
+
+### codemirror
+
+https://codemirror.net/
